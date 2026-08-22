6381106208d835c6ba8923587efb5b585d84989c
ZenKen-DA <gatilovvanya@yandex.ru>
2026-08-22 18:10:46 +0300

    Add files via upload

---
+++ extraction.sql
@@ -0,0 +1,17 @@
+SELECT 
+    "CreditScore",
+    "Geography",
+    "Gender",
+    "Age",
+    "Tenure",
+    "Balance",
+    "NumOfProducts",
+    "HasCrCard",
+    "IsActiveMember",
+    "EstimatedSalary",
+    "Complain",
+    "Satisfaction Score",
+    "Card Type",
+    "Point Earned",
+    "Exited"
+FROM customer_churn_records;
